05b1bfce6bdc72e887aba7b68494f65ab8bba151
Овчинников Вячеслав Дмитриевич <viachaslau_auchynnikau@epam.com>
2020-10-25 18:25:49 +0300

    feature: app template implemented

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesModule } from './modules/courses/courses.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    SharedModule,
+    CoursesModule,
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/modules/courses/components/courses-list/courses-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vc-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.scss']
+})
+export class CoursesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
