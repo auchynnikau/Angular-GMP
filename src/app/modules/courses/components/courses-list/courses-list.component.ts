ea9ed09dcef0eaca9031776987dfa1a5a1669e64
Овчинников Вячеслав Дмитриевич <viachaslau_auchynnikau@epam.com>
2020-11-08 22:27:19 +0300

    fix: changed usage of filter pipe

--- src/app/modules/courses/components/courses-list/courses-list.component.ts
+++ src/app/modules/courses/components/courses-list/courses-list.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { coursesMocks } from '../../../../shared/mocks/courses';
 import { CourseProps } from '../../../../shared/models/course';
+import { FilterPipe } from '../../../../shared/pipes/filter.pipe';
 
 @Component({
   selector: 'vc-courses-list',
   templateUrl: './courses-list.component.html',
   styleUrls: ['./courses-list.component.scss'],
+  providers: [FilterPipe],
 })
 export class CoursesListComponent implements OnInit {
+  constructor(private filter: FilterPipe) {}
+
   courses: CourseProps[] = [];
 
   @Input() searchQuery: string;
 
   ngOnInit(): void {
-    this.courses = coursesMocks;
+    this.courses = this.filter.transform(coursesMocks, this.searchQuery);
   }
 
   loadMore(): void {
