72bc0ffad5d01f7e9d7a372b905a1fb9b60ccf95
Viachaslau Auchynnikau <viachaslau_auchynnikau@epam.com>
2020-12-07 05:55:26 +0300

    feat: routing issues

--- src/app/modules/courses/pages/course-form/course-form.component.ts
+++ src/app/modules/courses/pages/course-form/course-form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CoursesService } from 'src/app/modules/courses/services/courses.service';
+import { CourseProps } from 'src/app/shared/models/course';
 
 @Component({
   selector: 'vc-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss'],
+  providers: [CoursesService],
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+  constructor(private coursesService: CoursesService, private route: ActivatedRoute) {}
+
+  course: CourseProps = {
+    id: '',
+    title: '',
+    duration: undefined,
+    creationDate: undefined,
+    isTopRated: false,
+    description: '',
+  };
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.course = this.coursesService.getCoursesItem(id);
+  }
+
   cancel(): void {
     console.log('cancel');
   }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../modules/material/material.module';
-
 import { FilterPipe } from './pipes/filter.pipe';
 import { OrderByPipe } from './pipes/orderBy.pipe';
 import { DurationPipe } from './pipes/duration.pipe';
-
 import { LogoComponent } from './components/logo/logo.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -29,6 +28,6 @@ const pipes = [DurationPipe, OrderByPipe, FilterPipe];
 @NgModule({
   declarations: [...pipes, ...components],
   exports: [...pipes, ...components],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
 })
 export class SharedModule {}
