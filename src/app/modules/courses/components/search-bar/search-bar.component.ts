0670a148d492871314213dc39674565062d1bba4
Viachaslau Auchynnikau <viachaslau_auchynnikau@epam.com>
2021-01-22 03:45:38 +0300

    feat: added rxjs for search field

--- src/app/modules/courses/components/search-bar/search-bar.component.ts
+++ src/app/modules/courses/components/search-bar/search-bar.component.ts
@@ -1,23 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { distinctUntilChanged, debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'vc-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) {}
 
-  searchValue: string;
+  private subject = new Subject<string>();
+  private searchValue: string;
 
-  handleSearch(): void {
+  ngOnInit() {
+    this.searchValue = this.queryParams.textFragment;
+    this.subject
+      .pipe(
+        debounceTime(500),
+        filter((value: string): boolean => this.filterSearchValue(value)),
+        distinctUntilChanged(),
+      )
+      .subscribe((value: string): void => this.updateQueryParams(value));
+  }
+
+  get queryParams() {
+    return { ...this.route.snapshot.queryParams };
+  }
+
+  onKeyUp(value: string): void {
+    this.subject.next(value);
+  }
+
+  filterSearchValue(value: string): boolean {
+    return value.length > 3 || value.length === 0;
+  }
+
+  updateQueryParams(value: string): void {
+    const { queryParams } = this;
+
+    if (value) {
+      queryParams.textFragment = value;
+    } else {
+      delete queryParams.textFragment;
+    }
+
     this.router.navigate([], {
-      queryParamsHandling: 'merge',
       relativeTo: this.route,
-      queryParams: {
-        textFragment: this.searchValue,
-      },
+      queryParams,
     });
   }
 }
